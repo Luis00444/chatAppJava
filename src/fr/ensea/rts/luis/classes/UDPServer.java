c06863c18e2545e10d00aa2142d4f3e70e3fe101
Luis <lmachado1209@gmail.com>
2024-10-23 17:24:27 +0200

    finished second draft of UDPServer

--- src/fr/ensea/rts/luis/classes/UDPServer.java
+++ src/fr/ensea/rts/luis/classes/UDPServer.java
@@ -4,36 +4,75 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.net.SocketException;
 import java.nio.BufferOverflowException;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
+/**
+ * Class UDPServer
+ * This class implements a simple server that listens for incoming datagrams.
+ * Then, it prints the received message if it is smaller than 1024 bytes.
+ * <p> </p>
+ * Usage example:
+ * <p></p>
+ * <code>
+ *    UDPServer server = new UDPServer(port);
+ *    server.launch();
+ *  </code>
+ */
 public class UDPServer {
     private final DatagramSocket socket;
     private boolean isListening;
+    static final int defaultPort = 1234;
+    private static final String hostAddress = "0.0.0.0";
+    private static final int minimumPortNumber = 0;
+    private static final int maximumPortNumber = 32767;
+    private static final int maximumReceivedMessageLength = 1024;
 
-    public UDPServer(int portToListen) throws IllegalArgumentException, IOException {
-        //todo: do the constructor
-        if (portToListen < 0 || portToListen > 32767){
-            throw new IllegalArgumentException("Port value should be in the range [0,16]");
+
+    /**
+     * Constructs an UDPServer object with a specific port to listen.
+     * This port should be a positive integer in the range of well know
+     * and registered ports
+     * @param portToListen port the server will listen incoming messages
+     * @throws IllegalArgumentException if the port number is not in the range
+     * @throws SocketException if the socket could not be opened
+     */
+    public UDPServer(int portToListen) throws IllegalArgumentException, SocketException {
+
+        if (portToListen < minimumPortNumber || portToListen > maximumPortNumber){
+            throw new IllegalArgumentException(
+                    "Port value should be in the range [" + minimumPortNumber + "," + maximumPortNumber + "]"
+            );
         }
-        InetSocketAddress address = new InetSocketAddress("0.0.0.0",portToListen);
+        InetSocketAddress address = new InetSocketAddress(hostAddress,portToListen);
         socket = new DatagramSocket(address);
         isListening = false;
     }
-    public UDPServer() throws IOException {
-        this(1234);
+
+
+      /**
+      * Constructs an UDPServer object with a default port
+      * @throws SocketException if the socket could not be opened
+      */
+    public UDPServer() throws SocketException {
+        this(defaultPort);
     }
 
+    /**
+     * Starts to listen the default address and the selected port.
+     * Incoming datagrams are shown preceded by <<<
+     */
     public void launch(){
         isListening = true;
-        int maxLengthOfString = 1024;
-        byte[] buffer = new byte[maxLengthOfString];
-        DatagramPacket packet = new DatagramPacket(buffer, maxLengthOfString);
+        byte[] buffer = new byte[maximumReceivedMessageLength];
+        DatagramPacket packet = new DatagramPacket(buffer, maximumReceivedMessageLength);
+        System.out.println(this);
         try {
-            while (true) {
+            while (!socket.isClosed()) {
                 socket.receive(packet);
-                if (packet.getLength() > maxLengthOfString) throw new BufferOverflowException();
+                if (packet.getLength() > maximumReceivedMessageLength) throw new BufferOverflowException();
                 byte[] received = packet.getData();
                 String message = new String(Arrays.copyOfRange(received,0,packet.getLength()), StandardCharsets.UTF_8);
                 System.out.print("<<< " + message);
@@ -54,7 +93,6 @@ public class UDPServer {
     }
 
     public static void main(String[] args) throws IllegalArgumentException, IOException {
-        //todo: create the main that accept only one argument: the port
         UDPServer server;
         if (args.length == 1){
             server = new UDPServer(Integer.parseInt(args[0]));
@@ -65,7 +103,6 @@ public class UDPServer {
         else{
             throw new IllegalArgumentException("Only accept one or zero arguments");
         }
-        System.out.println(server);
         server.launch();
     }
 
