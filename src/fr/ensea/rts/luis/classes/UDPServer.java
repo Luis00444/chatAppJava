8138a25bbfc1ea4db2ad53573254092298e3ced2
Luis <lmachado1209@gmail.com>
2024-10-23 14:25:19 +0200

    added the package

---
+++ src/fr/ensea/rts/luis/classes/UDPServer.java
@@ -0,0 +1,21 @@
+package fr.ensea.rts.luis.classes;
+
+public class UDPServer {
+    public UDPServer(int portToListen){
+        //todo: do the constructor
+    }
+    public UDPServer(){
+        this(1234);
+    }
+
+    public void launch(){
+        //todo: create this method
+    }
+
+    public void main(String[] args){
+        //todo: create the main that accept only one argument: the port
+    }
+
+
+
+}
