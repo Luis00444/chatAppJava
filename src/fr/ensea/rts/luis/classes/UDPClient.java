cc8912efdfd5471e7dda2f551d4526813290dbf4
DIas1999 <slim.shady.detroid@gmail.com>
2024-10-23 16:20:35 +0200

    created client

--- src/fr/ensea/rts/luis/classes/UDPClient.java
+++ src/fr/ensea/rts/luis/classes/UDPClient.java
@@ -5,15 +5,11 @@ import java.io.Console;
 public class UDPClient {
     public static void main(String[] args) {
 
-        String serverName = "localhost";  // e.g., "localhost"
-        int port = 8080;  // e.g., 8080
+        String serverName = "localhost";
+        int port = 8080;
 
         try {
-            // Get the InetAddress object for the server
-
             InetAddress serverAddress = InetAddress.getByName(serverName);
-
-            // Create a DatagramSocket to send the UDP datagram
             DatagramSocket socket = new DatagramSocket();
 
             // Use the Console class to read user input
@@ -26,7 +22,6 @@ public class UDPClient {
 
 
             while (true) {
-                // Read a line from the console
                 String userInput = console.readLine();
 
                 // Exit if the user types "exit"
@@ -35,7 +30,6 @@ public class UDPClient {
                     break;
                 }
 
-                // Convert the input string to bytes (UTF-8 encoding)
                 byte[] data = userInput.getBytes("UTF-8");
 
                 // Create a DatagramPacket to send the data
