957d6cba68eb6d6d71aea3d119f57efdb2ee939e
Luis <lmachado1209@gmail.com>
2024-10-23 14:42:31 +0200

    added the package

--- src/fr/ensea/rts/luis/classes/UDPServer.java
+++ src/fr/ensea/rts/luis/classes/UDPServer.java
@@ -1,5 +1,7 @@
 package fr.ensea.rts.luis.classes;
 
+import javax.print.AttributeException;
+
 public class UDPServer {
     public UDPServer(int portToListen){
         //todo: do the constructor
@@ -12,8 +14,11 @@ public class UDPServer {
         //todo: create this method
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IllegalArgumentException{
         //todo: create the main that accept only one argument: the port
+        if (args.length > 1) {
+            throw new IllegalArgumentException("Only accept one or zero arguments");
+        }
     }
 
     @Override
