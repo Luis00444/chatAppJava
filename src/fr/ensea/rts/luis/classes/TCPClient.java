b771dc971ed74e25c4fedd6497f23ea1e28aef1e
DIas1999 <slim.shady.detroid@gmail.com>
2024-11-20 14:54:56 +0100

    Unfinished

--- src/fr/ensea/rts/luis/classes/TCPClient.java
+++ src/fr/ensea/rts/luis/classes/TCPClient.java
@@ -1,8 +1,9 @@
 package fr.ensea.rts.luis.classes;
 import javax.net.ssl.SNIServerName;
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
-import java.io.Console;
+import java.nio.charset.StandardCharsets;
+
 /****************************************
 *TCP client to read and send message
  ***************************************/
@@ -11,12 +12,44 @@ public class TCPClient {
         String Host = args[0];
         int port = Integer.parseInt(args[1]);
 
-        try{
+        Socket tcp_socket;
+        try {
             InetAddress serverAddr = InetAddress.getByName(Host);
-            Socket tcp_socket = new Socket(serverAddr, port);
-
+            tcp_socket = new Socket(serverAddr, port);
+            System.out.println("Connected to TCP server.");
         } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
+            throw new UnknownHostException("Host not found." + e.getMessage());
         }
+
+        /* Sending text to Server */
+        OutputStream outputStream = tcp_socket.getOutputStream();
+        OutputStreamWriter outputStreamWriter = new OutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+        PrintWriter printWriter = new PrintWriter(outputStreamWriter, true);
+        printWriter.println("Hello from Client");
+
+        //Receive data from Server
+        InputStream inputStream = tcp_socket.getInputStream();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String serverResponse = bufferedReader.readLine();
+        System.out.println(serverResponse);
+
+        // Use console to read user input
+        Console console = System.console();
+        if (console == null) {
+            System.out.println("No console available");
+            return;
+        }
+        System.out.println("Type your message (type 'exit' to quit):");
+
+        //Get text from User until types <ctrl>+D
+        while (true) {
+            String userInput = console.readLine();
+            if (userInput == null || (userInput.length() > 1024)) {
+                System.out.println("Client exiting.");
+                break;
+            }
+        }
+
+        byte[] data = userInput.getBytes("UTF-8");
     }
 }
