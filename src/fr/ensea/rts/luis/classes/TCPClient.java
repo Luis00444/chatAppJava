4c37ca6c100cf86ce1be9746ed691faed0747365
Luis <lmachado1209@gmail.com>
2024-12-08 19:29:14 +0100

    Put some comments, put something in main, and minor corrections. Created an interface Launchable for all servers and clients for the main

--- src/fr/ensea/rts/luis/classes/TCPClient.java
+++ src/fr/ensea/rts/luis/classes/TCPClient.java
@@ -8,17 +8,16 @@ import java.io.InputStreamReader;
  * Creates a TCP client to send and receive messages
  * Usage java TCPClient.java "server address" "port"
  */
-public class TCPClient {
-private static Socket tcp_socket;
+public class TCPClient implements Launchable{
+    private final Socket tcpSocket;
 
     /**TCP INput/OUTput Manager - provides message exchange between Client and Server
-     * @param tcp_socket
      * @throws IOException - throws exception if connection was interrupted
      */
-    public void Tcp_IO_manager(Socket tcp_socket) throws IOException {
+    public void launch() throws IOException {
         Console console = System.console();
-        BufferedReader serverReader = new BufferedReader(new InputStreamReader(tcp_socket.getInputStream(), StandardCharsets.UTF_8)); // Server response
-        PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(tcp_socket.getOutputStream(), StandardCharsets.UTF_8), true); // Send data to server;
+        BufferedReader serverReader = new BufferedReader(new InputStreamReader(tcpSocket.getInputStream(), StandardCharsets.UTF_8)); // Server response
+        PrintWriter serverWriter = new PrintWriter(new OutputStreamWriter(tcpSocket.getOutputStream(), StandardCharsets.UTF_8), true); // Send data to server;
         System.out.println("Type your message and press Enter. Press <CTRL>+D(Z for windows) to exit.");
         while (true) {
             try{
@@ -44,14 +43,14 @@ private static Socket tcp_socket;
     public TCPClient(String host, int port ) throws IOException {
         try {
             InetAddress serverAddr = InetAddress.getByName(host);
-            tcp_socket = new Socket(serverAddr, port);
+            tcpSocket = new Socket(serverAddr, port);
             System.out.println("Connected to TCP server.");
         } catch (UnknownHostException e) {
             throw new UnknownHostException("Host not found." + e.getMessage());
         }
     }
     // Closes TCP socket after the end of connection
-    public void closeConnection(Socket tcpSocket) throws IOException {
+    public void closeConnection() throws IOException {
         tcpSocket.close();
         System.out.println("Connection closed.");
     }
@@ -62,9 +61,9 @@ private static Socket tcp_socket;
         TCPClient tcpClient = new TCPClient(Host, port); //Create tcp socket
 
         try {
-            tcpClient.Tcp_IO_manager(tcp_socket);
+            tcpClient.launch();
         } finally {
-            tcpClient.closeConnection(tcp_socket);
+            tcpClient.closeConnection();
         }
     }
 }
